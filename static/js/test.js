a0ec3c4d54d5fa9483774343020e7267f1bdc91f
hjfeldy <harry.feldman99@gmail.com>
2021-03-24 13:55:37 -0500

    js file-location

--- static/js/test.js
+++ static/js/test.js
@@ -1,1 +1,1 @@
-d3.json('https://cors.io/?https://nfl-flask.herokuapp.com/year/2016/three_cone&forty_yard').then(data => console.log(data))
+d3.json('/year/2016/three_cone&forty_yard').then(data => console.log(data))
