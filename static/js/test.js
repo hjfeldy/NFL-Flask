0ffeff2d42e67365a6b25a9f9229b264e8fe276f
hjfeldy <harry.feldman99@gmail.com>
2021-03-24 15:39:42 -0500

    js file-location

--- static/js/test.js
+++ static/js/test.js
@@ -1,8 +1,11 @@
 // API calls with d3:
 
-// So we actually do need to use flask to deploy the html, because browsers don't let you bring in data from a foreign source. This means that the data must come from the same place as the html itself (ie, the heroku server). This isn't a prolem. All you guys have to do is make sure all your html files are in the "templates" directory, and that all your javascript is in the "static/js" directory. Then, when you put your scripts into your html, the format will be like this: <script src=/static/js/SCRIPT_NAME></script>. Similarly, when you call the API routes, you will not specify a url, but only a path. Index.html combined with this javascript file (test.js) serve as an example of this
+// So we actually do need to use flask to deploy the html, because browsers don't let you bring in data from a foreign source. This means that the data must come from the same place as the html itself (ie, the heroku server).
+//This isn't a prolem. All you guys have to do is make sure all your html files are in the "templates" directory, and that all your javascript is in the "static/js" directory. Then, when you put your scripts into your html, the format will be like this:
+//<script src=/static/js/SCRIPT_NAME></script>. Similarly, when you call the API routes, you will not specify a url, but only a path. Index.html combined with this javascript file (test.js) serve as an example of this
 
-// For example, if I go to 'https://nfl-flask.herokuapp.com/id/Mehdi Abdesmad' in my browser, I will get a json object response. However, if I want to get that same json object response WITHIN the app, I will just do: d3.json('/id/Mehdi Abdesmad')
+// For example, if I go to 'https://nfl-flask.herokuapp.com/id/Mehdi Abdesmad' in my browser, I will get a json object response. 
+// However, if I want to get that same json object response WITHIN the app, I will just do: d3.json('/id/Mehdi Abdesmad')
 
 
 // Some examples:
